468de678720aadb16bae7f9126c9d39cb4f05326
ogatensor <drew.tensor@gmail.com>
2024-05-01 17:48:47 -0400

    (monorepo:refactor): update ESLint config to 9.1.1

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,36 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default {
-  root: true, 
-  overrides: [
-    {languageOptions: { globals: globals.node }},
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-  ]
+const configurations = {
+  indexer: [
+    {
+      files: ["indexer/**/*.js"],
+      languageOptions: { globals: globals.node }
+    },
+    {
+      files: ["indexer/**/*.js"],
+      ...pluginJs.configs.recommended
+    },
+    {
+      files: ["indexer/**/*.js"],
+      ...tseslint.configs.recommended
+    },
+  ],
+  sniper: [
+    {
+      files: ["sniper/**/*.js"],
+      languageOptions: { globals: globals.node }
+    },
+    {
+      files: ["sniper/**/*.js"],
+      ...pluginJs.configs.recommended
+    },
+    {
+      files: ["sniper/**/*.js"],
+      ...tseslint.configs.recommended
+    },
+  ],
+  ignores: ["**/*.test.js"], 
 };
+
+export default configurations;
